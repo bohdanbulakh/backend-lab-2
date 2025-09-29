ac4e57b70669494fabaf2642d25de6c34be203f1
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-09-29 22:18:23 +0300

    added memory check to endpoint

--- src/modules/health-check/health-check.controller.ts
+++ src/modules/health-check/health-check.controller.ts
@@ -1,22 +1,35 @@
 import { Controller, Get } from '@nestjs/common';
 import { DiskHealthIndicator, HealthCheckService } from '@nestjs/terminus';
+import { ExtendedMemoryIndicator } from './indicators/extended-memory.indicator';
+import { HealthCheckStatus } from '@nestjs/terminus/dist/health-check/health-check-result.interface';
 
 @Controller()
 export class HealthCheckController {
   constructor(
     private readonly healthCheckService: HealthCheckService,
     private readonly diskHealthIndicator: DiskHealthIndicator,
+    private readonly memoryIndicator: ExtendedMemoryIndicator,
   ) {}
 
   @Get('/healthcheck')
   public async healthcheck() {
-    const { status } = await this.healthCheckService.check([
-      () =>
-        this.diskHealthIndicator.checkStorage('storage', {
-          path: '/',
-          thresholdPercent: 0.95,
-        }),
-    ]);
+    let status: HealthCheckStatus;
+    try {
+      const result = await this.healthCheckService.check([
+        () =>
+          this.diskHealthIndicator.checkStorage('storage', {
+            path: '/',
+            thresholdPercent: 0.95,
+          }),
+        () =>
+          this.memoryIndicator.checkHeap('heap', {
+            thresholdPercent: 0.9,
+          }),
+      ]);
+      status = result.status;
+    } catch {
+      status = 'error';
+    }
 
     return {
       status,
--- src/modules/health-check/health-check.module.ts
+++ src/modules/health-check/health-check.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { HealthCheckController } from './health-check.controller';
 import { TerminusModule } from '@nestjs/terminus';
+import { ExtendedMemoryIndicator } from './indicators/extended-memory.indicator';
 
 @Module({
   imports: [TerminusModule],
   controllers: [HealthCheckController],
+  providers: [ExtendedMemoryIndicator],
+  exports: [ExtendedMemoryIndicator],
 })
 export class HealthCheckModule {}
