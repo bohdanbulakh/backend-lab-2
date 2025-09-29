d6574012c48ee230bfffb4e4d24a145eb64bcd55
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-09-30 02:38:23 +0300

    deleted ram check to avoid unstability of health check

--- src/modules/health-check/health-check.controller.ts
+++ src/modules/health-check/health-check.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { DiskHealthIndicator, HealthCheckService } from '@nestjs/terminus';
-import { ExtendedMemoryIndicator } from './indicators/extended-memory.indicator';
 import { HealthCheckStatus } from '@nestjs/terminus/dist/health-check/health-check-result.interface';
 
 @Controller()
@@ -8,7 +7,6 @@ export class HealthCheckController {
   constructor(
     private readonly healthCheckService: HealthCheckService,
     private readonly diskHealthIndicator: DiskHealthIndicator,
-    private readonly memoryIndicator: ExtendedMemoryIndicator,
   ) {}
 
   @Get('/healthcheck')
@@ -20,10 +18,6 @@ export class HealthCheckController {
           this.diskHealthIndicator.checkStorage('storage', {
             path: '/',
             thresholdPercent: 0.95,
-          }),
-        () =>
-          this.memoryIndicator.checkHeap('heap', {
-            thresholdPercent: 0.9,
           }),
       ]);
       status = result.status;
--- src/modules/health-check/health-check.module.ts
+++ src/modules/health-check/health-check.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HealthCheckController } from './health-check.controller';
 import { TerminusModule } from '@nestjs/terminus';
-import { ExtendedMemoryIndicator } from './indicators/extended-memory.indicator';
 
 @Module({
   imports: [TerminusModule],
   controllers: [HealthCheckController],
-  providers: [ExtendedMemoryIndicator],
-  exports: [ExtendedMemoryIndicator],
 })
 export class HealthCheckModule {}
