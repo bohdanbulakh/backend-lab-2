5b0cb22662e5f3f4d5cf992816634bf09da81397
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-09-30 23:28:05 +0300

    added dto for record

---
+++ src/common/dto/create-record.dto.ts
@@ -0,0 +1,6 @@
+export class CreateRecordDto {
+  name: string;
+  userId: string;
+  categoryId: string;
+  sum: number;
+}
---
+++ src/common/dto/get-record-query.dto.ts
@@ -0,0 +1,4 @@
+export class GetRecordQueryDto {
+  categoryId?: string;
+  userId?: string;
+}
