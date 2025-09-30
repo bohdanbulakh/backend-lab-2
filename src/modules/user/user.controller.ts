d2abd542e129397e4d635185e2d99aa22f9f241b
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-09-30 23:56:08 +0300

    fixed user endpoint names

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,26 +3,26 @@ import { UserService } from './user.service';
 import { UserResponse } from '../../common/responses/user.response';
 import { UserEntity } from '../../dao/user/user.entity';
 
-@Controller('/user')
+@Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get()
+  @Get('/users')
   getAll(): UserResponse[] {
     return this.userService.getAll();
   }
 
-  @Get(':userId')
+  @Get('/user/:userId')
   getById(@Param('userId') id: string): UserResponse {
     return this.userService.getById(id);
   }
 
-  @Post()
+  @Post('user')
   create(@Body() data: UserEntity): UserResponse {
     return this.userService.create(data);
   }
 
-  @Delete(':userId')
+  @Delete('/user/:userId')
   updateById(@Param('userId') id: string): UserResponse {
     return this.userService.deleteById(id);
   }
