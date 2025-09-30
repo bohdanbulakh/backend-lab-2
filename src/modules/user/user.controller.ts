2be2406045776dc476dc18b49c4af2c77fb88657
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-09-30 21:39:42 +0300

    created user controller

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserResponse } from '../../common/responses/user.response';
+import { UserEntity } from '../../dao/user/user.entity';
+
+@Controller('/user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getAll(): UserResponse[] {
+    return this.userService.getAll();
+  }
+
+  @Get(':userId')
+  getById(@Param('userId') id: string): UserResponse {
+    return this.userService.getById(id);
+  }
+
+  @Post()
+  create(@Body() data: UserEntity) {
+    return this.userService.create(data);
+  }
+
+  @Delete(':userId')
+  updateById(@Param('userId') id: string): UserResponse {
+    return this.userService.deleteById(id);
+  }
+}
