de7242535f3f9ec16ba303dada8b5be3856ec018
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-10-03 01:12:26 +0300

    added class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get<ConfigService>(ConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(config.port);
 
   console.log(`Server url: http://localhost:${config.port}`);
