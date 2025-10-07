5b5a954566501209299e0adc47a3a972340b68fc
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-10-07 22:41:25 +0300

    fix password input

--- src/common/dto/login.dto.ts
+++ src/common/dto/login.dto.ts
@@ -1,9 +1,9 @@
 import {
   IsNotEmpty,
   IsString,
-  IsStrongPassword,
   Length,
   MaxLength,
+  MinLength,
 } from 'class-validator';
 
 export class LoginDto {
@@ -13,7 +13,7 @@ export class LoginDto {
   username: string;
 
   @IsNotEmpty()
-  @IsStrongPassword({ minNumbers: 8, minSymbols: 2, minLength: 10 })
+  @MinLength(8)
   @MaxLength(50)
   password: string;
 }
